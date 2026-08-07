6a871b1ecccee7f47ac03ab0cc7cad2db1d6caff
LavKush <152675661+rtlavkush07@users.noreply.github.com>
2026-08-07 16:37:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const MONGO_URL = process.env.MONGO_URL;
 
 // CORS Middleware for API
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://demochat-1.onrender.com',
   credentials: true
 }));
 
