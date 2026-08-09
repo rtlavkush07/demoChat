b182626415a28647e3f16fa31b8cfbaa9aed6bca
LavKush <152675661+rtlavkush07@users.noreply.github.com>
2026-08-09 09:19:14 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3002',
+        target: 'https://demochat-1.onrender.com',
         changeOrigin: true,
         
       }
