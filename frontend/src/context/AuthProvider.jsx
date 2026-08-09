11bf25a64ed021219db9acd1d224b6cc65f1043f
LavKush <152675661+rtlavkush07@users.noreply.github.com>
2026-08-09 10:31:26 +0530

    Update AuthProvider.jsx

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,26 +1,29 @@
-import React, { createContext, useContext,useState } from 'react'
- import Cookies from 'js-cookie';
-
+import React, { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
-export  const AuthProvider = ({children}) => {
-    const initialUserState = Cookies.get("jwt") || localStorage.getItem("user");
-    const [AuthUser, setAuthUser] = useState(() => {
-        if (initialUserState) {
-            try {
-                return JSON.parse(initialUserState);
-            } catch (error) {
-                console.error("Error parsing user data from localStorage:", error);
-                return undefined;
-            }
-        }
-        return undefined;
-    });
+
+export const AuthProvider = ({ children }) => {
+  const [AuthUser, setAuthUser] = useState(() => {
+    const user = localStorage.getItem("user");
+
+    if (!user) {
+      return null;
+    }
+
+    try {
+      return JSON.parse(user);
+    } catch (error) {
+      console.error("Error parsing user:", error);
+      localStorage.removeItem("user");
+      return null;
+    }
+  });
+
   return (
     <AuthContext.Provider value={[AuthUser, setAuthUser]}>
-        {children}
+      {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
 export const useAuth = () => useContext(AuthContext);
