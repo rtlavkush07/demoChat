2781a08d303418fa2699c8195c552c87ba83bd85
LavKush <152675661+rtlavkush07@users.noreply.github.com>
2026-08-11 16:15:05 +0530

    Update Users.jsx

--- frontend/src/home/left/Users.jsx
+++ frontend/src/home/left/Users.jsx
@@ -8,7 +8,9 @@ const [allUsers, loading] =  userGetAllUsers();
   const realUsers = allUsers?.allUsers || [];
   // console.log(realUsers);
   return (
-  <div className="py-2 flex-1 overflow-y-auto">
+  <div  className="py-2 flex-1 overflow-y-auto"
+        style={{ maxHeight: "calc(84vh - 10vh)" }}
+        >
            {/* console.log("Rendering :", user); */}
     
    {realUsers.map((user,index)=>{
