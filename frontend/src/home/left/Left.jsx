f531e17b7af80b6d5642f3aa38c90b4969f220f5
LavKush <152675661+rtlavkush07@users.noreply.github.com>
2026-08-11 15:34:21 +0530

    Update Left.jsx

--- frontend/src/home/left/Left.jsx
+++ frontend/src/home/left/Left.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
 import Search from './Search';
-import User from './Users';
+import User from './Users'; // Keeping your import as User from Users
+import useConversation from '../../statemanage/useConversation.js'; // Added for responsive toggle
 
 function Left () {
+  const { selectedConversation } = useConversation();
+
   return (
-    <div className=" w-[30%] bg-black text-white">
-      <h1 className='font-bold text-3xl p-2 px-11'>Chats</h1>
-    <Search/>  
-    <hr />
-    <User/>
+    <div className={`w-[85%] md:w-[30%] bg-black/30 text-white border-r border-gray-700/50 flex flex-col h-full ${selectedConversation ? "hidden md:flex" : "flex"}`}>
+      <h1 className='font-bold text-2xl p-4 border-b border-gray-700/50'>Chats</h1>
+      <Search/>  
+      <User/>
     </div>
   )
 }
